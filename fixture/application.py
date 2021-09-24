f649f10cadc2fd9cc590e07b08a2d8b9da8b07dd
Vasele <wasp143@ua.ru>
2021-09-24 17:17:03 +0300

    add testlogin

---
+++ fixture/application.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from fixture.session import SessionHelper
+
+
+
+class Application:
+
+    def __init__(self, browser, baseurl):
+        if browser == "firefox":
+           self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        self.wd.implicitly_wait(0.5)
+        self.session = SessionHelper(self)
+        self.baseurl = baseurl
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def open_homepage(self):
+        wd = self.wd
+        wd.get(self.baseurl)
+
+    def tearDown(self):
+        self.wd.quit()
---
+++ fixture/db.py
@@ -0,0 +1,43 @@
+import pymysql.cursors
+from model.group import Group
+from model.contact import Contact
+
+
+class DbFixture:
+
+    def __init__(self, host, name, user, password):
+        self.host = host
+        self.name = name
+        self.user = user
+        self.password = password
+        self.connection = pymysql.connect(host=host, database=name, user=user, password=password, autocommit=True)
+
+    def get_group_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
+            for row in cursor:
+                (id, name, header, footer) = row
+                list.append(Group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return list
+
+    def get_contact_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname, lastname, address, home, phone2, mobile, work, email, email2, email3"
+                           "  from addressbook where deprecated='000-00-00 00:00:00'")
+            for row in cursor:
+                (id, firstname, lastname, address, home, phone2, mobile, work, email, email2, email3) = row
+                list.append(Contact(id=str(id), name=firstname, lastname=lastname, address=address,
+                                    phone_home=home, phone_secondary=phone2, phone_mobile=mobile,
+                                    phone_work=work, email=email, email2=email2, email3=email3))
+        finally:
+            cursor.close()
+        return list
+
+    def destroy(self):
+        self.connection.close()
