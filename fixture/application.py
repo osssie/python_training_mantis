97b7951d6cb7c28891ee0b8678f7162cb81ab96c
Vasele <wasp143@ua.ru>
2021-09-28 20:09:09 +0300

    POP3 protocol

--- fixture/application.py
+++ fixture/application.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.project import ProjectHelper
 from fixture.james import JamesHelper
+from fixture.mail import MailHelper
+from fixture.signup import SignupHelper
 
 
 class Application:
@@ -19,6 +21,8 @@ class Application:
         self.session = SessionHelper(self)
         self.project = ProjectHelper(self)
         self.james = JamesHelper(self)
+        self.mail = MailHelper(self)
+        self.signup = SignupHelper(self)
         self.config = config
         self.baseurl = config['web']['baseUrl']
 
